a696bdc869b4a7b0c9bccc14bc48eaf5004d0b76
Daniel Castillejo <devcastillejo@gmail.com>
2016-03-24 00:25:46 -0500

    Minimal changes to the Login screen

--- client/components/Login.js
+++ client/components/Login.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import * as actionCreators from '../actions';
 
 export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    const redirectRoute = this.props.location.query.next || '/login';
+    this.state = {
+      username: '',
+      password: '',
+      redirectTo: redirectRoute
+    }
+  }
+
+  login = (e) => {
+    this.props.actions.loginUser(this.sate.username, this.state.password, this.state.redirectTo)
+  }
+
   render() {
     return (
       <div id="login" className="ui middle aligned center aligned grid">
@@ -14,13 +30,23 @@ export default class Login extends Component {
               <div className="field">
                 <div className="ui left icon input">
                   <i className="user icon"></i>
-                  <input type="text"  name="username" placeholder="Username"/>
+                  <input type="text"
+                         name="username"
+                         placeholder="Usuario"
+                         value={this.state.username}
+                         onChange={this.handleUsernameChange}
+                  />
                 </div>
               </div>
               <div className="field">
                 <div className="ui left icon input">
                   <i className="lock icon"></i>
-                  <input type="password" name="password" placeholder="Password"/>
+                  <input type="password"
+                         name="password"
+                         placeholder="Contraseña"
+                         value={this.state.password}
+                         onChange={this.handlepasswordChange}
+                  />
                 </div>
               </div>
               <div className="ui fluid large blue submit button">Log in</div>
